90ca0753227214ca22034361efe1a81a7edb738b
aysf <ansufw@gmail.com>
2023-04-04 13:46:35 +0200

    add project 3

--- 2_html_with_ts/script.ts
+++ 2_html_with_ts/script.ts
@@ -1,11 +1,11 @@
-const btn = document.querySelector("button");
+const btn = document.querySelector("button")! as HTMLElement;
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 btn.addEventListener("click", function() {
-    console.log(add(+input1.value, +input2.value));
+    console.log(sum(+input1.value, +input2.value));
 });
 
-function add(num1: number, num2: number) {
+function sum(num1: number, num2: number) {
     return num1 + num2;
 }
---
+++ 3_setup_project/README.md
@@ -0,0 +1,11 @@
+# How to setup a project
+
+1. init project with `npm init`
+2. install lite-server as a development only dependency with command `npm install --save-dev lite-server`
+3. add `"start": "lite-server"` to `package.json` under `script` tag.
+4. open terminal and run `start`
+
+# How to re-use
+
+1. clone this project folder
+2. install all dependencies `npm install`
