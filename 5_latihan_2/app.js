8670348be0d0c877a02e2f5eb3905ba80ed183bc
aysf <ansufw@gmail.com>
2023-04-05 22:39:57 +0200

    add latihan 5

---
+++ 5_latihan_2/app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 2;
+var num2 = 9;
+var result = add(num1, num2);
+console.log(result);
---
+++ 5_latihan_2/app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const num1 = 2;
+const num2 = 9;
+
+const result = add(num1, num2);
+console.log(result)
