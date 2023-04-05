62953826cfdbd7c99923070be104e38c7c7bfe38
aysf <ansufw@gmail.com>
2023-04-05 22:46:32 +0200

    error case

--- 5_latihan_2/app.js
+++ 5_latihan_2/app.js
@@ -1,7 +1,10 @@
 function add(n1, n2) {
+    if (typeof n1 !== 'number') {
+        throw new Error('Input Salah!');
+    }
     return n1 + n2;
 }
-var num1 = 2;
+var num1 = '2';
 var num2 = 9;
 var result = add(num1, num2);
 console.log(result);
--- 5_latihan_2/app.ts
+++ 5_latihan_2/app.ts
@@ -1,8 +1,11 @@
 function add(n1: number, n2: number) {
+    if (typeof n1 !== 'number') {
+        throw new Error('Input Salah!')
+    }
     return n1 + n2;
 }
 
-const num1 = 2;
+const num1 = '2';
 const num2 = 9;
 
 const result = add(num1, num2);
