ce7f83adf0b1c63f39c407855a741b8958370eba
aysf <ansufw@gmail.com>
2023-04-07 01:45:31 +0200

    add latihan 8

---
+++ 8_latihan_type_inf/app.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1;
+num1 = 9;
+console.log(add(5, num1));
---
+++ 8_latihan_type_inf/app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number) {
+
+    return n1 + n2
+}
+
+let num1: number;
+num1 = 9;
+
+console.log(add(5,num1))
