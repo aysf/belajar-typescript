4a14052fc9dc6416aca81a4c06360f00de2fe8de
aysf <ansufw@gmail.com>
2023-04-07 02:00:31 +0200

    add latihan 8

---
+++ 9_latihan_object/app.js
@@ -0,0 +1,16 @@
+// const person = {
+//     name: 'Ananto',
+//     age: 40
+// }
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: "Ananto",
+//     age: 30
+// }
+var person = {
+    name: "Budi",
+    age: 23
+};
+console.log(person.name);
---
+++ 9_latihan_object/app.ts
@@ -0,0 +1,18 @@
+// better
+// const person = {
+//     name: 'Ananto',
+//     age: 40
+// }
+
+
+// just for example
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "Ananto",
+    age: 30
+}
+
+
+console.log(person.name);
